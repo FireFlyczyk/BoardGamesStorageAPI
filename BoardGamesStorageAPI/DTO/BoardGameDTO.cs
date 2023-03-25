15d171e2a7438a9b8f88240436b3a4f684b6c52b
FireFlyczyk <107504085+FireFlyczyk@users.noreply.github.com>
2023-03-25 14:18:38 +0100

    cooment removed

--- BoardGamesStorageAPI/DTO/BoardGameDTO.cs
+++ BoardGamesStorageAPI/DTO/BoardGameDTO.cs
@@ -7,7 +7,7 @@
         public int MaximumPlayers { get; set; }
         public int PlayTimeInMinutes { get; set; }
         public int AgeLimit { get; set; }
-       // public int YearOfProduction { get; set; }
+        public int YearOfProduction { get; set; }
         public string Publisher { get; set; }
         public string Author { get; set; }
 
